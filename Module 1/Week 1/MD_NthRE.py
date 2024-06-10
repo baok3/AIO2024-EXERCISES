e58f99898b9e95cf3fa574c2e9674c6d6f4bd90f
quanbaonguyen03 <quanbaonguyen03@gmail.com>
2024-06-10 20:34:58 +0700

    sửa lại lỗi hàm

--- Module 1/Week 1/MD_NthRE.py
+++ Module 1/Week 1/MD_NthRE.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     y_hat = float(input('Nhập y_hat: '))
     n = int(input('Nhập n: '))
     p = int(input('Nhập p: '))
-    print('Mean Difference of nth Root Error =', mean_difference_of_nth_root_error(y, y_hat, n, p))
+    print('Mean Difference of nth Root Error =', MD_NthRE(y, y_hat, n, p))
